293767422232871000cceb5ebb9c32cbf6dd919d
TheStaticTurtle <tuglersamuel@gmail.com>
2021-04-05 15:32:45 +0200

    Added checks / redirects for non-existant content

--- pages/course/_slugCourse/_slugChapter/index.vue
+++ pages/course/_slugCourse/_slugChapter/index.vue
@@ -11,19 +11,27 @@
 		name: "Course",
 		layout: "course",
 
-		async asyncData ({ $content, params, store}) {
-			console.log(params)
-			let courseConfig = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.json' }).fetch()
-			if(courseConfig.length === 1) courseConfig = courseConfig[0]
-			else courseConfig = null
+		async asyncData ({ $content, params, store, redirect}) {
+			let courseConfig = null
+			try {
+				courseConfig = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.json' }).fetch()
+				if(courseConfig.length === 1) courseConfig = courseConfig[0]
+			} catch (e) {
+				redirect("/")
+			}
 			store.commit("course/setConfig",courseConfig)
 
-			let chapters = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.md' }).fetch()
-			chapters = chapters.sort(function(a, b) {
-				if (a.order < b.order) return -1;
-				if (a.order > b.order) return 1;
-				return 0;
-			})
+			let chapters = []
+			try {
+				chapters = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.md' }).fetch()
+				chapters = chapters.sort(function(a, b) {
+					if (a.order < b.order) return -1;
+					if (a.order > b.order) return 1;
+					return 0;
+				})
+			} catch (e) {
+				redirect("/")
+			}
 			store.commit("course/setChapters",chapters)
 
 			const chapter = chapters.find((x)=>{ return x.slug===params.slugChapter })
--- pages/course/_slugCourse/index.vue
+++ pages/course/_slugCourse/index.vue
@@ -11,20 +11,29 @@
 		layout: "course",
 
 		async asyncData ({ $content, params, store, redirect}) {
-			let courseConfig = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.json' }).fetch()
-			if(courseConfig.length === 1) courseConfig = courseConfig[0]
-			else courseConfig = null
+			let courseConfig = null
+			try {
+				courseConfig = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.json' }).fetch()
+				if(courseConfig.length === 1) courseConfig = courseConfig[0]
+			} catch (e) {
+				redirect("/")
+			}
 			store.commit("course/setConfig",courseConfig)
 
-			let chapters = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.md' }).fetch()
-			chapters = chapters.sort(function(a, b) {
-				if (a.order < b.order) return -1;
-				if (a.order > b.order) return 1;
-				return 0;
-			})
+			let chapters = []
+			try {
+				chapters = await $content("courses", { deep: true }, params.slugCourse).where({ extension: '.md' }).fetch()
+				chapters = chapters.sort(function(a, b) {
+					if (a.order < b.order) return -1;
+					if (a.order > b.order) return 1;
+					return 0;
+				})
+			} catch (e) {
+				redirect("/")
+			}
 			store.commit("course/setChapters",chapters)
 
-			if(chapters.length>0) redirect( "/course/"+ courseConfig.dir.replace("/courses/","") +'/'+chapters[0].slug )
+			if(!!courseConfig && chapters.length>0) redirect( "/course/"+ courseConfig.dir.replace("/courses/","") +'/'+chapters[0].slug )
 
 			return {
 				courseConfig,
