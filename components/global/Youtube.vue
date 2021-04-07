3634489d19b1c6c3992d72d1d538bca9dbb2e6a6
TheStaticTurtle <tuglersamuel@gmail.com>
2021-04-07 21:52:55 +0200

    Added iframe demo

---
+++ components/global/Youtube.vue
@@ -0,0 +1,21 @@
+<template>
+	<div style="width: 100%; display: flex; align-items: center; justify-content: center; padding: 8px">
+		<iframe :src="'https://www.youtube.com/embed/'+content" width="533" height="300" frameborder="0" allowfullscreen=""></iframe>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Youtube",
+		props: {
+			content: {
+				required: true,
+				type: String
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ content/courses/example-course/Iframes.md
@@ -0,0 +1,12 @@
+---
+title: Iframes
+subtitle: Display other sites
+icon: mdi-square-outline
+order: 80
+---
+
+## Iframe (HTML)
+<iframe src="https://www.youtube.com/embed/xcJtL7QggTI" width="533" height="300" frameborder="0" allowfullscreen=""></iframe>
+
+## Youtube compenent
+<Youtube content="xcJtL7QggTI"></Youtube>
